ad09d1ccb49d2683ec9b5c0c8a1097f9a3a83da9
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2022-01-02 20:01:52 +0100

    Cany images

--- create_data.py
+++ create_data.py
@@ -9,7 +9,9 @@ from utils.grab_screen import grab_screen
 while True:
     last_time = time.time()
     image = grab_screen(region=(550, 300, 1349, 749))
-
+    # This line makes edge detection worse in "Art of Rally"
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.Canny(image, threshold1=119, threshold2=250)
     image = cv2.resize(image, (224, 224))
     cv2.imshow("Image", image)
     cv2.waitKey(1)
