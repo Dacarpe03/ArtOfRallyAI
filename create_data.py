93940a644e72e373d071e605c70e39766657abf9
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2022-01-02 19:40:38 +0100

    First commit

---
+++ create_data.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+import time
+import os
+# 1280 x 800 game resolution
+# image = grab_screen(region=(550, 300, 1349, 749))
+from utils.grab_screen import grab_screen
+
+while True:
+    last_time = time.time()
+    image = grab_screen(region=(550, 300, 1349, 749))
+
+    image = cv2.resize(image, (224, 224))
+    cv2.imshow("Image", image)
+    cv2.waitKey(1)
+    print('loop took {} seconds'.format(time.time() - last_time))
