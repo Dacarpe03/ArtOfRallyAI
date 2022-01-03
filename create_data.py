d0d268aa7e12bb911f94fffe480709da9a7297fe
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2022-01-03 17:40:16 +0100

    Add code to save the images as np data

--- create_data.py
+++ create_data.py
@@ -3,12 +3,29 @@ import cv2
 import time
 import os
 # 1280 x 800 game resolution
+# SECOND CAMERA IN GAME
 # image = grab_screen(region=(550, 300, 1349, 749))
 from utils.grab_screen import grab_screen
 from utils.getkeys import key_check
 
+file_name = "C:/Users/dcarm/ArtOfRallyAI/data/training_data.npy"
+file_name2 = "C:/Users/dcarm/ArtOfRallyAI/data/keys_data.npy"
+
+
+def save_data(image_data, targets):
+    np.save(file_name, image_data)
+    np.save(file_name2, targets)
+
+
 image_data = []
 keys_pressed = []
+
+while True:
+    keys = key_check()
+    if keys == "H":
+        break
+keys = "N"
+
 while True:
     last_time = time.time()
     image = grab_screen(region=(550, 300, 1349, 749))
@@ -28,3 +45,5 @@ while True:
         break
     print(f"{keys} pressed")
     # print('loop took {} seconds'.format(time.time() - last_time))
+
+save_data(image_data, keys_pressed)
